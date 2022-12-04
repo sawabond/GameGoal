454bbdc1be7a3092d4af3de39ab42617b479542d
dovhopolyi-do/56-rEb_G3Kh-Nm90 <dovhopolyi_do@groupbwt.com>
2022-12-04 14:24:54 +0200

    Implementing uploading files

--- src/ui/src/pages/ImportMembers/ImportMembers.js
+++ src/ui/src/pages/ImportMembers/ImportMembers.js
@@ -1,6 +1,51 @@
-import React from 'react';
-function ImportMembers() {
-  return <h1>Hello</h1>;
-}
+import React, { useState } from 'react';
+import axios from 'axios';
+import Header from '../../components/Header';
+import { Button } from '@material-ui/core';
+export default function ImportMembers() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
 
-export default ImportMembers;
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append('File', selectedFile);
+
+    axios
+      .post('https://localhost:7184/api/User/import-members', {
+        method: 'POST',
+        body: formData,
+      })
+      .then((result) => {
+        console.log('Success:', result);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <>
+      <Header />
+      <input type="file" name="file" onChange={changeHandler} />
+      {isFilePicked ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <Button color="primary" onClick={handleSubmission}>
+          Submit
+        </Button>
+      </div>
+    </>
+  );
+}
