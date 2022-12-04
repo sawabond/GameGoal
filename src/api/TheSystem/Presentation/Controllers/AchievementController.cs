c8482eae708f58ff1e72a1e809d4238ace5e6df7
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-12-04 19:56:57 +0200

    Added IoT simulation for completing not to smoke achievement

--- src/api/TheSystem/Presentation/Controllers/AchievementController.cs
+++ src/api/TheSystem/Presentation/Controllers/AchievementController.cs
@@ -1,4 +1,6 @@
-﻿using Application.Achievements.Commands.CreateAchievement;
+﻿using Application.Achievements.Commands.CompleteAchievement;
+using Application.Achievements.Commands.CreateAchievement;
+using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -6,7 +8,7 @@ using Presentation.Requests;
 
 namespace Presentation.Controllers;
 
-[Authorize]
+[Authorize(Roles = RoleConstants.User)]
 public sealed class AchievementController : AuthorizedApiController
 {
     public AchievementController(ISender sender) : base(sender)
@@ -15,6 +17,7 @@ public sealed class AchievementController : AuthorizedApiController
     }
 
     [HttpPost]
+    [Authorize(Roles = RoleConstants.Company)]
     public async Task<IActionResult> CreateAchievement([FromBody] CreateAchievementRequest request)
     {
         var command = new CreateAchievementCommand(
@@ -22,6 +25,18 @@ public sealed class AchievementController : AuthorizedApiController
             request.Name, 
             request.Description, 
             request.IsNegative);
+
+        var result = await _sender.Send(command);
+
+        return result.IsSuccess
+            ? Ok(result)
+            : BadRequest(result.Errors);
+    }
+
+    [HttpPost("{name}")]
+    public async Task<IActionResult> CompleteAchievement(string name)
+    {
+        var command = new CompleteAchievementCommand(UserId, name);
 
         var result = await _sender.Send(command);
 
